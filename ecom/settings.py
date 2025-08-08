ebe59fb5a4a1a37553cb138f1b0acf505f5af24f
Shivam Rathod <shivamrathod145@gmail.com>
2025-08-08 14:26:22 +0530

    Fix ALLOWED_HOSTS to include djangocart.onrender.com domain

--- ecom/settings.py
+++ ecom/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-$zjv(uj@oub%r&b%5@3r3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.railway.app,.render.com').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,.railway.app,.render.com,djangocart.onrender.com').split(',')
 
 # Application definition
 
